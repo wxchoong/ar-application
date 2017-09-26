96a7f1c95d7eb7cc1b9e5b5216a6c64e7e6c5488
wxchoong <32188551+wxchoong@users.noreply.github.com>
2017-09-26 09:39:41 +0800

    Update barChart.js

--- barChart.js
+++ barChart.js
@@ -6,7 +6,7 @@ var xhttp = new XMLHttpRequest();
 			var response = JSON.parse(xhttp.responseText);
 			var data = response.data;
 			
-			var color = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+			var color = ['hotpink', 'orange', 'yellow', 'green', 'blue', 'purple']
 			var x = -0.25;
 			
 			var scene = document.querySelector('a-scene');
