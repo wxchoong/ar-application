90bb13ddff96a7b56b52a46e9dd03715a1dd7d29
wxchoong <32188551+wxchoong@users.noreply.github.com>
2017-09-28 11:55:49 +0800

    Update barChart.js

--- barChart.js
+++ barChart.js
@@ -8,22 +8,22 @@ var xhttp = new XMLHttpRequest();
 			
 			var color = ['deeppink', 'orangered', 'gold', 'yellowgreen', 'deepskyblue', 'darkorchid']
 			var x = -0.25;
-			var z = -0.8;
+			var z = -0.5;
 				
 			var scene = document.querySelector('a-scene');
 			
 			var title = document.createElement('a-text');
 					title.setAttribute('value', "Temperature");
 					title.setAttribute('color', 'red');
-					title.setAttribute('position', {x:-0.65, y:0.05, z:0});
+					title.setAttribute('position', '-0.65 0.05 0.5');
 					title.setAttribute('rotation', '-90 90 0');
 					title.setAttribute('scale', '0.5 0.5 0.5');
 					scene.appendChild(title);
 			
 			var user_alert = document.createElement('a-circle');
 				user_alert.setAttribute('color', 'lawngreen');
-				user_alert.setAttribute('radius', '0.05');
-				user_alert.setAttribute('position', '-0.65 0.05 -0.8');
+				user_alert.setAttribute('radius', '0.045');
+				user_alert.setAttribute('position', '-0.65 0.05 -0.25');
 				user_alert.setAttribute('rotation', '-90 0 0');
 				scene.appendChild(user_alert);
 				
@@ -38,12 +38,12 @@ var xhttp = new XMLHttpRequest();
 				 }
 				 
 				if (max > 100){
-					alarm.setAttribute('color', 'crimson');
+					user_alert.setAttribute('color', 'crimson');
 				}
 				
 				var chart = document.createElement('a-box');
 					chart.setAttribute('color', color[n]);
-					chart.setAttribute('position', {x:x, y:0, z:chart_scale/2});
+					chart.setAttribute('position', {x:x, y:0, z:chart_scale/2+0.5});
 					chart.setAttribute('width', '0.1');
 					chart.setAttribute('height', '0.1');
 					chart.setAttribute('depth' , chart_scale);
@@ -54,16 +54,16 @@ var xhttp = new XMLHttpRequest();
 					
 					var axis = document.createElement('a-box');
 						axis.setAttribute('color', 'red');
-						axis.setAttribute('position', {x:-0.35, y:0.02, z:i*(-0.005)-0.025});
+						axis.setAttribute('position', {x:-0.35, y:0.02, z:i*(-0.005)+0.475});
 						axis.setAttribute('width', '0.01');
 						axis.setAttribute('height', '0.01');
-						axis.setAttribute('depth', (i*(-0.01))-0.05);
+						axis.setAttribute('depth', i*(-0.01)-0.05);
 						scene.appendChild(axis);
 					
 					if(i > 0){
 						var value_indicator = document.createElement('a-box');
 							value_indicator.setAttribute('color', 'red');
-							value_indicator.setAttribute('position', {x:-0.35, y:0.02, z: i*(-0.01)});
+							value_indicator.setAttribute('position', {x:-0.35, y:0.02, z: i*(-0.01)+0.5});
 							value_indicator.setAttribute('scale', '0.05 0.01 0.01');
 							scene.appendChild(value_indicator);
 						}
@@ -71,7 +71,7 @@ var xhttp = new XMLHttpRequest();
 					var value = document.createElement('a-text');
 						value.setAttribute('value', i);
 						value.setAttribute('color', 'red');
-						value.setAttribute('position', {x:-0.42, y:0.02, z:i*(-0.01)});
+						value.setAttribute('position', {x:-0.42, y:0.02, z:i*(-0.01)+0.5});
 						value.setAttribute('rotation', '-90 0 0');
 						value.setAttribute('align', 'right');
 						value.setAttribute('scale', '0.2 0.2 0.2');
@@ -81,7 +81,7 @@ var xhttp = new XMLHttpRequest();
 					var name = document.createElement('a-text');
 						name.setAttribute('value', data[n].name);
 						name.setAttribute('color', 'red');
-						name.setAttribute('position', {x:x-0.05, y:0.1, z:0.05});
+						name.setAttribute('position', {x:x-0.05, y:0.1, z:0.55});
 						name.setAttribute('rotation', '-90 0 0');
 						name.setAttribute('scale', '0.3 0.3 0.3');
 						scene.appendChild(name);
@@ -89,15 +89,15 @@ var xhttp = new XMLHttpRequest();
 					var legend = document.createElement('a-text');
 						legend.setAttribute('value', data[n].name + ': ' + data[n].legend);
 						legend.setAttribute('color', 'red');
-						legend.setAttribute('position', {x:0.8, y:0.1, z:z});
+						legend.setAttribute('position', {x:0.75, y:0.1, z:z});
 						legend.setAttribute('rotation', '-90 0 0');
 						legend.setAttribute('scale', '0.2 0.2 0.2');
 						scene.appendChild(legend);
 					
 					var legend_color = document.createElement('a-box');
 						legend_color.setAttribute('color', color[n]);
-						legend_color.setAttribute('position', {x:0.76, y:0.1, z:z});
-						legend_color.setAttribute('scale', '0.04 0 0.04');
+						legend_color.setAttribute('position', {x:0.73, y:0.1, z:z});
+						legend_color.setAttribute('scale', '0.03 0 0.03');
 						scene.appendChild(legend_color);
 						
 				
